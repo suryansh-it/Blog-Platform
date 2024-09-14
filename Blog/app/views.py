fa82c1596202bec90aa2d5992dd65dbb2e4d73d2
Suryansh Sharma <suryansharma09@gmail.com>
2024-09-14 19:20:03 +0530

    dynaic url added

--- Blog/app/views.py
+++ Blog/app/views.py
@@ -1,16 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 posts =[]
 def home(request):
     html = ""
     for post in posts :
         html += f'''
-                    <div><h1>{post['id']} - {post['title']}</h1>
+                    <div>
+                    <a href= "/post/{post['id']}/">
+                    <h1>{post['id']} - {post['title']}</h1></a>
                     <p>{post['content']}</p>
                     </div>'''
 
     return HttpResponse(html)
 
 def post(request,id):
-    return HttpResponse(id)
+    for post in posts:
+        if post['id'] == id:
+            post_dict = post  
+            break  
+    
+    html = f'''<h1>{post_dict['title']}</h1>
+               <p>{post_dict['content']}</p>'''
+    return HttpResponse(html)
 # Create your views here.
