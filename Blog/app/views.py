6a4c2f78a4d9fc6e82faec02e8cb3f3532c7b212
Suryansh Sharma <suryansharma09@gmail.com>
2024-09-14 18:56:05 +0530

    initial functions added

--- Blog/app/views.py
+++ Blog/app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
+posts =[]
+def home(request):
+    html = ""
+    for post in posts :
+        html += f'''
+                    <div><h1>{post['id']} - {post['title']}</h1>
+                    <p>{post['content']}</p>
+                    </div>'''
+
+    return HttpResponse(html)
+
+def post(request,id):
+    return HttpResponse(id)
 # Create your views here.
