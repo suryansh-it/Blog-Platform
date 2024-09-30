20c121fff66e2a2493025f9419b8ee555f22eeee
Suryansh Sharma <suryansharma09@gmail.com>
2024-10-01 01:56:42 +0530

    url setup done for views

--- Blog/app/urls.py
+++ Blog/app/urls.py
@@ -1,2 +1,8 @@
 from django.urls import path
 from . import views
+
+urlpatterns =[
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.blog_detail, name='blog_detail'),
+    path('post/new/', views.create_post, name='create_post'),
+]
