3795ef6d4b649507198bf70439757341642ec00a
Suryansh Sharma <suryansharma09@gmail.com>
2024-09-30 19:59:45 +0530

    added post model

--- Blog/app/models.py
+++ Blog/app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class POsts(models.Model):
+    title = models.CharField(max_length= 200) 
+    content = models.TextField()
+    dated= models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self) :
+        return self.title  #return title when the obj is printed
