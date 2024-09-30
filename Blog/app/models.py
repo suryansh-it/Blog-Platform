496ce649cf49081eb36d247d0d3a1f653fa6b7ec
Suryansh Sharma <suryansharma09@gmail.com>
2024-09-30 20:35:35 +0530

    added author to  post model

--- Blog/app/models.py
+++ Blog/app/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
-class POsts(models.Model):
+class Posts(models.Model):
     title = models.CharField(max_length= 200) 
     content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     dated= models.DateTimeField(auto_now_add=True)
 
 
