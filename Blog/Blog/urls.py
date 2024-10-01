a6512544703fb5ec65d1e499789dea62719ebefb
Suryansh Sharma <suryansharma09@gmail.com>
2024-10-01 13:46:53 +0530

    adding base teplat

--- Blog/Blog/urls.py
+++ Blog/Blog/urls.py
@@ -25,12 +25,17 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('' , include('app.urls')),# Include the app's URLs
     path('', views.homepage_view, name='home'),  # Root URL, points to homepage
+    path('posts/', views.post_list_view, name='post_list'),  # Post list page
+    path('accounts/signup/', views.signup, name='signup'),
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    
 ]
 
 
 # Auth views (login, logout, signup)
-urlpatterns += [
-    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
-    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('accounts/signup/', views.signup, name='signup'),
-]
+# urlpatterns += [
+#     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+#     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+#     path('accounts/signup/', views.signup, name='signup'),
+# ]
